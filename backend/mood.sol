1fe6aaaaa04cb44f2cb7a6960a69ce1c03d91e81
jayPreak <jayesh.preak2003@gmail.com>
2023-05-31 20:07:50 +0530

    dapp hehe

--- backend/mood.sol
+++ backend/mood.sol
@@ -1,2 +1,16 @@
 //SPDX -// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+contract MoodDiary{
+    // string public test = "Hello World";
+    string mood;
+
+    function setMood(string memory _mood) public {
+        mood = _mood;
+    }
+
+    function getMood() public view returns (string memory) {
+        return mood;
+    }
+
+}
